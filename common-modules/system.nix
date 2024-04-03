564896c6939d41b0061da815d05fe27480eb6e36
Blake Ashley <blake@ashleyjr.com>
2024-04-03 15:21:03 -0700

    making progress on install-system.yaml

--- common-modules/system.nix
+++ common-modules/system.nix
@@ -66,6 +66,7 @@
     # Shell environments
     fish # Friendly Interactive SHell, a smart and user-friendly command line shell
     python311Packages.pexpect # Pure Python Expect-like module
+    expect # A tool for automating interactive applications
   ];
 
 
--- secrets/secrets.nix
+++ secrets/secrets.nix
@@ -5,7 +5,7 @@ let
   blake = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIK29aC0ZvTjltZcJkPHSGn01Zlhywr1QJZVtKQ8U3YU1 blake@ashleyjr.com";
 
   ## hypervisors
-  hv-2 = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIDFZH2lAXrME17k5TyToUgi+7MTiUW/xOfliMelY//wr  hv-2";
+  hv-2 = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIE7jD15lsyJv5rndjiVJj4sk0eyBvAgFkTLm3+LZnfXN  hv-2";
 
   ## workstations
 
