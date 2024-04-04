2adad6e883f615c247b0fb88ef36961d58b2c261
Blake Ashley <blake@ashleyjr.com>
2024-04-04 15:22:02 -0700

    Add SSH public key for hv-2.

--- secrets/secrets.nix
+++ secrets/secrets.nix
@@ -5,7 +5,7 @@ let
   blake = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIK29aC0ZvTjltZcJkPHSGn01Zlhywr1QJZVtKQ8U3YU1 blake@ashleyjr.com";
 
   ## hypervisors
-  hv-2 = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIDfR6LBbwX1oca/gJ0yhXnHOFLyxVl11P57iWixPXdLy  hv-2";
+  hv-2 = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIFsGUN/ziBOhutvvNaZfsd5SkMfWPx/I2OJLbY+Fci5W  hv-2";
 
   ## workstations
 
