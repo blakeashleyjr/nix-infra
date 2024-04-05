47b095bcc07d079f55ca2d2083d8cb7ae4528262
Blake Ashley <blake@ashleyjr.com>
2024-04-04 19:00:01 -0700

    adding hv-2 secrets

--- secrets/secrets.nix
+++ secrets/secrets.nix
@@ -5,7 +5,7 @@ let
   blake = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIK29aC0ZvTjltZcJkPHSGn01Zlhywr1QJZVtKQ8U3YU1 blake@ashleyjr.com";
 
   ## hypervisors
-  hv-2 = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIHcdMNsm3Rgpx8Ci1Pc+yWcvIheU3AmvxPzNLETmPM+s root@nixos";
+  hv-2 = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIEUA3ZF0cZ0evIhT1dzuwDp8XIhFA4qkEu92qd0HlpzE root@nixos";
 
   ## workstations
 
