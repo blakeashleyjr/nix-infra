3267ad34ca5440f96d90f363cee4a1b278d25bbc
Blake Ashley <blake@ashleyjr.com>
2024-05-08 21:41:53 -0700

    working on k3s

--- hypervisors/hv-modules/hv-users.nix
+++ hypervisors/hv-modules/hv-users.nix
@@ -4,12 +4,17 @@
     isNormalUser = true;
     description = "serveradmin";
     shell = pkgs.fish;
-    extraGroups = [ "wheel" ];
+    extraGroups = [ "wheel" "serveradmin" ];
     packages = with pkgs; [
 
     ];
     openssh.authorizedKeys.keys = [ "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIK29aC0ZvTjltZcJkPHSGn01Zlhywr1QJZVtKQ8U3YU1 blake@ashleyjr.com" ];
 
+  };
+
+  # Define the serveradmin group
+  users.groups.serveradmin = {
+    gid = 1000;
   };
 
   programs.fish.enable = true;
