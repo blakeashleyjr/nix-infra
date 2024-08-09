94df6220c3aca1c27a79d9559c4f6572770eccdb
Blake Ashley <blake@ashleyjr.com>
2024-08-09 01:14:58 +0000

    Getting ares up on Flake v5

--- common-modules/system.nix
+++ common-modules/system.nix
@@ -10,7 +10,7 @@
     "nextdns-config-stamp-ws.age".file = ../secrets/nextdns-config-stamp-ws.age;
   };
 
-  age.identityPaths = [ "/etc/ssh/ssh_host_ed25519_key.pub" ];
+  age.identityPaths = [ "/etc/ssh/ssh_host_ed25519_key" ];
 
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
